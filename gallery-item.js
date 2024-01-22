b1d2425472a71cd4c7e858e70a1fa0738d564358
johnpaulo27 <drivenpaulo@gmail.com>
2024-01-22 22:15:15 +0800

    hw7 2nd commit

--- gallery-item.js
+++
@@ -1,8 +0,0 @@
-function openModal(imageSrc) {
-  document.getElementById('modalImage').src = imageSrc;
-  document.getElementById('myModal').style.display = 'flex';
-}
-
-function closeModal() {
-  document.getElementById('myModal').style.display = 'none';
-}
